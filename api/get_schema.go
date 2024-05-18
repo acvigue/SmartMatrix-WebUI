45d3bd6bfba91dcf0f707843ecd820e8b2a1c2c6
Aiden Vigue <aiden@vigue.me>
2024-05-18 18:37:54 -0400

    ok

---
+++ api/get_schema.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"tidbyt.dev/pixlet/runtime"
+)
+
+type HandlerStruct struct {
+  applet_source string
+}
+ 
+func Handler(w http.ResponseWriter, r *http.Request) {
+  decoder := json.NewDecoder(r.Body)
+
+	var body HandlerStruct
+	err := decoder.Decode(&body)
+
+	if err != nil {
+		fmt.Printf("error parsing request body: %s\n", err)
+    os.Exit(1)
+	}
+
+  //download the applet source
+	req, err := http.NewRequest(http.MethodGet, body.applet_source, nil)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: response body: %s\n", resBody)
+
+  app, err := runtime.NewApplet("applet", resBody)
+  if err != nil {
+    fmt.Printf("error creating applet: %s\n", err)
+    os.Exit(1)
+  }
+
+  fmt.Fprintf(w, "%s", string(app.SchemaJSON))
+}
